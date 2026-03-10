bdbb16180fc275132e1a3059aeea616f5e4964be
alperen buğra <alperenbugra@192.168.1.34>
2026-03-10 17:15:19 +0300

    readme

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Loader = () => {
   return (
-    <div className='loader-container'>
-        <div className='loader'>
-            <div className='loader-ring'/>
-            <span>Yükleniyor...</span>
-        </div>
+    <div className="loader-container">
+      <div className="loader">
+        <div className="loader-ring" />
+        <span>Yükleniyor..</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
